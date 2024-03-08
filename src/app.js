6d3c8202375dc6beb5e34280c378ece316b86ca6
Lars Gregersen <lagr@videndjurs.dk>
2024-03-08 09:07:24 +0100

    first

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import Initialize from "./Initialize";
+import "../css/style.scss";
+
+// **  IIFE: Immediately Invoked Function Expression  */
+
+(function () {
+
+  console.log("starting the cool->app");
+
+  let init = new Initialize();
+
+})();
